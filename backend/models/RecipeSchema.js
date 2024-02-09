7fe70dd3c29ebb793cd5fd98c59423d2bd75d3d5
Alistair Abao <abaolester@gmail.com>
2024-02-09 18:54:10 +0100

    RecipeSchema created

---
+++ backend/models/RecipeSchema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const RecipeSchema = new Schema(
+  {
+    recipeName: {
+      type: String,
+      required: true,
+    },
+    recipeIngredients: [
+      {
+        ingredientName: {
+          type: String,
+          required: true,
+        },
+        ingredientQty: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    recipeInstructions: {
+      type: string,
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId(),
+      ref: UserSchema,
+    },
+  },
+  { timestamps: true } //creates createdAt property for every entry
+);
+
+export const RecipeModel = mongoose.model("RecipeModel", RecipeSchema);
