f63be680257424c9e7c3f214da6e1857e7622d3b
Alistair Abao <abaolester@gmail.com>
2024-02-13 00:10:33 +0100

    hashing user password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,6 +4,8 @@ import "express-async-errors";
 import { ExpressError } from "../errors/ExpressError.js";
 import mongoose from "mongoose";
 import { UserModel } from "../models/UserSchema.js";
+//bcrypt to hash pswd.
+import bcrypt from "bcrypt";
 
 //register
 export const userRegister = async (req, res) => {
@@ -14,9 +16,17 @@ export const userRegister = async (req, res) => {
   const isAdmin = await UserModel.countDocuments(0);
   //create a new property for role
   req.body.role = isAdmin === 0 ? "admin" : "user";
+  //hashing a password
+  const salt = bcrypt.genSaltSync(12);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt); //accepts the password from the form(req.body) and the salt round created.
+  req.body.password = hashedPassword;
   const newUser = await UserModel.create(req.body);
+  console.log(newUser);
   if (!newUser) {
     throw new ExpressError("Cannot create user");
   }
   res.status(200).json({ message: "New user registered", newUser });
 };
+
+//logging in
+export const userLogin = (req, res) => {};
