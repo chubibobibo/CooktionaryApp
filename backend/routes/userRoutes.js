33e58381fd3270e7f85bcf325e4ab044d57c6ccd
Alistair Abao <abaolester@gmail.com>
2024-02-13 15:06:23 +0100

    login user functionality with jwt and cookies

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 //import controllers
-import { userRegister } from "../controllers/userControllers.js";
+import { userRegister, userLogin } from "../controllers/userControllers.js";
 const router = express.Router();
 
 //input validation
 import { validateRegister } from "../middleware/inputValidation.js";
 
 router.post("/register", validateRegister, userRegister);
+router.post("/login", userLogin);
 
 export default router;
