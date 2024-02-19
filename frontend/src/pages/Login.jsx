9fd4dd04ac69ed7418a5e33bf001416ecf38feb5
Alistair Abao <abaolester@gmail.com>
2024-02-19 14:28:42 +0100

    error page and conditional 404 error rendering

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,15 @@
+//using Link
+import { Link } from "react-router-dom";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login
+      <div>
+        No account yet?
+        <Link to={"/register"}> Register</Link>
+      </div>
+    </div>
+  );
 }
 export default Login;
